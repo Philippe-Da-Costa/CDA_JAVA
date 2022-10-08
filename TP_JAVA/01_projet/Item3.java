59bcd1a7c88bc820d88c497ad956d8ce35feee09
philippe <97304026+Philippe-Da-Costa@users.noreply.github.com>
2022-10-08 11:46:40 +0200

    re

--- TP_JAVA/01_projet/Item3.java
+++ TP_JAVA/01_projet/Item3.java
@@ -8,3 +8,8 @@ public class Item3 {
         System.out.println("valeur de e: "+e);
     }
 }
+
+//Explication le byte est sur 1 octet.
+//valeur du byte  -128 a 127
+// quand on redefinit la valeur de c on sort de la capacité d'un byte.
+// effectivement 70 + 70 font 140 nous somme au dessus de la capacité d un byte.
---
+++ TP_JAVA/01_projet/Item3.java.bak
@@ -0,0 +1,10 @@
+public class Item3 {
+    public static void main(String[] args) {
+       byte c = 50;
+       byte d = 70;
+	   byte e;
+	   c=70;
+	   e = (byte)( c + d);
+        System.out.println("valeur de e: "+e);
+    }
+}
